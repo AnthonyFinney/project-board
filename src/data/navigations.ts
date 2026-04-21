3439e4d5be13ef972fb4899bca127dd6f0b27fd8
AnthonyFinney <anthonyrockfinnie0@gmail.com>
2026-04-21 18:32:08 +0600

    first commit

---
+++ src/data/navigations.ts
@@ -0,0 +1,183 @@
+import type { NavigationType } from "@/types"
+
+export const navigationsData: NavigationType[] = [
+  {
+    title: "Dashboards",
+    items: [
+      {
+        title: "Analytics",
+        href: "/dashboards/analytics",
+        iconName: "ChartPie",
+      },
+      {
+        title: "CRM",
+        href: "/dashboards/crm",
+        iconName: "ChartBar",
+      },
+      {
+        title: "eCommerce",
+        href: "/dashboards/ecommerce",
+        iconName: "ShoppingCart",
+      },
+    ],
+  },
+  {
+    title: "Pages",
+    items: [
+      {
+        title: "Landing",
+        href: "/pages/landing",
+        iconName: "LayoutTemplate",
+      },
+      {
+        title: "Pricing",
+        href: "/pages/pricing",
+        iconName: "CircleDollarSign",
+      },
+      {
+        title: "Payment",
+        href: "/pages/payment",
+        iconName: "CreditCard",
+      },
+      {
+        title: "Help Center",
+        href: "#",
+        iconName: "Headset",
+      },
+      {
+        title: "Settings",
+        href: "/pages/account/settings",
+        iconName: "UserCog",
+      },
+      {
+        title: "Profile",
+        href: "/pages/account/profile",
+        iconName: "User",
+      },
+      {
+        title: "Fallback",
+        iconName: "Replace",
+        items: [
+          {
+            title: "Coming Soon",
+            href: "/pages/coming-soon",
+          },
+          {
+            title: "Not Found 404",
+            href: "/pages/not-found-404",
+          },
+          {
+            title: "Unauthorized 401",
+            href: "/pages/unauthorized-401",
+          },
+          {
+            title: "Maintenance",
+            href: "/pages/maintenance",
+          },
+        ],
+      },
+      {
+        title: "Authentication",
+        iconName: "LogIn",
+        items: [
+          {
+            title: "Forgot Password",
+            href: "/forgot-password",
+          },
+          {
+            title: "New Password",
+            href: "/new-password",
+          },
+          {
+            title: "Verify Email",
+            href: "/verify-email",
+          },
+          {
+            title: "Register",
+            href: "/register",
+          },
+          {
+            title: "Sign In",
+            href: "/sign-in",
+          },
+        ],
+      },
+      {
+        title: "Products",
+        iconName: "Package",
+        items: [
+          {
+            title: "List",
+            href: "/",
+          },
+          {
+            title: "Add",
+            href: "/",
+          },
+          {
+            title: "Category",
+            href: "/",
+          },
+        ],
+      },
+      {
+        title: "Orders",
+        iconName: "ShoppingBasket",
+        items: [
+          {
+            title: "List",
+            href: "/",
+          },
+          {
+            title: "Details",
+            href: "/",
+          },
+        ],
+      },
+      {
+        title: "Customers",
+        iconName: "Users",
+        items: [
+          {
+            title: "List",
+            href: "/",
+          },
+          {
+            title: "Details",
+            href: "/",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Apps",
+    items: [
+      {
+        title: "Email",
+        href: "/apps/email",
+        iconName: "AtSign",
+      },
+      {
+        title: "Chat",
+        href: "/apps/chat",
+        iconName: "MessageCircle",
+      },
+      {
+        title: "Calendar",
+        href: "/apps/calendar",
+        iconName: "Calendar",
+      },
+      {
+        title: "Kanban",
+        href: "/apps/kanban",
+        iconName: "Grid2x2",
+      },
+      {
+        title: "Todo",
+        href: "#",
+        iconName: "ListTodo",
+      },
+    ],
+  },
+]
---
+++ src/lib/get-dictionary.ts
@@ -0,0 +1,21 @@
+// More info: https://nextjs.org/docs/app/building-your-application/routing/internationalization#localization
+import "server-only"
+
+import type { LocaleType } from "@/types"
+
+const dictionaries = {
+  en: () =>
+    import("@/data/dictionaries/en.json").then((module) => module.default),
+  ar: () =>
+    import("@/data/dictionaries/ar.json").then((module) => module.default),
+}
+
+export async function getDictionary(locale: LocaleType) {
+  if (!dictionaries[locale]) {
+    console.warn(`Dictionary for locale "${locale}" not found. Falling back to default locale "en".`)
+    return dictionaries.en()
+  }
+  return dictionaries[locale]()
+}
+
+export type DictionaryType = Awaited<ReturnType<typeof getDictionary>>
